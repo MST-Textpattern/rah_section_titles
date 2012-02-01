7ceaea9aa731457e5ae92e12943928d49fc6c674
Jukka Svahn <web@rahinaa.biz>
2012-02-01 07:07:22 +0200

    Adding v0.2 from SVN.

--- code.php
+++ code.php
@@ -1,55 +1,43 @@
 <?php	##################
 	#
 	#	rah_section_titles-plugin for Textpattern
-	#	version 0.1.2
+	#	version 0.2
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
 	###################
 
-	if (@txpinterface == 'admin') {
-		register_callback('rah_section_titles','article');
-	}
+	if (@txpinterface == 'admin')
+		register_callback('rah_section_titles','admin_side','head_end');
 
 	function rah_section_titles() {
-		global $vars;
-		extract(gpsa(array('view','from_view','step','ID')));
-		if(!$view || gps('save') || gps('publish')) {
-			$view = 'text';
-		}
-		if (!$step) $step = "create";
-		if ($step == "edit" && $view == "text" && !empty($ID) && $from_view != 'preview' && $from_view != 'html') $pull = true;
-		else {
-			$pull = false;
-			if ($from_view=='preview' or $from_view=='html') {
-				$store_out = array();
-				$store = unserialize(base64_decode(ps('store')));
-				foreach($vars as $var) if (isset($store[$var])) $store_out[$var] = $store[$var];
-			} else $store_out = gpsa($vars);
-			extract($store_out);
-		}
-		if(!$from_view && !$pull) $Section = getDefaultSection();
-		if(is_numeric(gps('ID'))) $Section = fetch('Section','textpattern','ID',gps('ID'));
-		if(ps('Section')) $Section = ps('Section');
-		$code = rah_section_popup($Section);
+		
+		global $event;
+		
+		if($event != 'article')
+			return;
+		
+		$rs = 
+			safe_rows(
+				'title,name',
+				'txp_section',
+				"name != 'default' order by title ASC"
+			);
+		
+		if(!$rs)
+			return;
+		
+		foreach($rs as $a)
+			$out[] = 
+				'		$("select[name=Section] option[value='.$a['name'].']").html("'.
+					str_replace('"','\"',$a['title']).
+				'");'.n;
+		
 		echo 
+			n.
 			'<script language="javascript" type="text/javascript">'.n.
 			'	$(document).ready(function() {'.n.
-			'		$("#section").remove();'.n.
-			'		$("label[for=section]").after(\''.$code.'\');'.n.
+					implode('',$out).
 			'	});'.n.
-			'</script>';
-	}
-
-	function rah_section_popup($selected) {
-		$rs = safe_rows_start('title, name', 'txp_section',"name != 'default' order by title ASC");
-		if ($rs){
-			$out[] = '<br /><select id="section" name="Section">';
-			while ($a = nextRow($rs)) {
-				extract($a);
-				$out[] = str_replace("'",'','<option value="'.htmlspecialchars($name).'"'.(($selected == $name) ? ' selected="selected"' : '').'>'.htmlspecialchars($title).'</option>');
-			}
-			$out[] = '</select>';
-			return implode('', $out);
-		}
-	} ?>
+			'</script>'.n;
+	}?>
