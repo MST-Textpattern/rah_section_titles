b956f4543481418f4f49ad120deadf08320d85ea
Jukka Svahn <web@rahinaa.biz>
2012-02-01 07:08:05 +0200

    Adding v0.3 from SVN.

--- code.php
+++ code.php
@@ -1,13 +1,17 @@
 <?php	##################
 	#
 	#	rah_section_titles-plugin for Textpattern
-	#	version 0.2
+	#	version 0.3
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
-	###################
+	#	Copyright (C) 2011 Jukka Svahn <http://rahforum.biz>
+	#	Licensed under GNU Genral Public License version 2
+	#	http://www.gnu.org/licenses/gpl-2.0.html
+	#
+	##################
 
-	if (@txpinterface == 'admin')
+	if(@txpinterface == 'admin')
 		register_callback('rah_section_titles','admin_side','head_end');
 
 	function rah_section_titles() {
@@ -21,7 +25,7 @@
 			safe_rows(
 				'title,name',
 				'txp_section',
-				"name != 'default' order by title ASC"
+				"name != 'default' ORDER BY title ASC"
 			);
 		
 		if(!$rs)
@@ -29,15 +33,18 @@
 		
 		foreach($rs as $a)
 			$out[] = 
-				'		$("select[name=Section] option[value='.$a['name'].']").html("'.
+				'		$("select[name=Section] option[value='.$a['name'].']").text("'.
 					str_replace('"','\"',$a['title']).
 				'");'.n;
 		
 		echo 
 			n.
-			'<script language="javascript" type="text/javascript">'.n.
+			'<script type="text/javascript">'.n.
+			'	<!--'.n.
 			'	$(document).ready(function() {'.n.
 					implode('',$out).
 			'	});'.n.
+			'	-->'.n.
 			'</script>'.n;
-	}?>
+	}
+?>
