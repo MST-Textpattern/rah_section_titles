32a04cba9d14ae0f97d3f46ebe2594536dcf8eef
Jukka Svahn <web@rahinaa.biz>
2012-12-21 20:51:50 +0200

    Programming style and comment cleanup.

--- rah_section_titles.php
+++ rah_section_titles.php
@@ -3,12 +3,12 @@
 /**
  * Rah_section_titles plugin for Textpattern CMS
  *
- * @author Jukka Svahn
- * @date 2008-
+ * @author  Jukka Svahn
+ * @date    2008-
  * @license GNU GPLv2
- * @link http://rahforum.biz/plugins/rah_section_titles
+ * @link    http://rahforum.biz/plugins/rah_section_titles
  *
- * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -16,28 +16,31 @@
 	register_callback('rah_section_titles', 'article_ui', 'section');
 
 /**
- * Replaces the section list
- * @param string $event
- * @param string $step
- * @param string $default
- * @param array $data
- * @return HTML
+ * Replaces the section list.
+ *
+ * @param  string $event
+ * @param  string $step
+ * @param  string $default
+ * @param  array  $data
+ * @return string HTML
  */
 
-	function rah_section_titles($event, $step, $default, $data) {
-		
+	function rah_section_titles($event, $step, $default, $data)
+	{
 		$rs = safe_rows('name, title', 'txp_section', "name != 'default' ORDER BY title ASC");
-		
-		if(!$rs) {
+
+		if (!$rs)
+		{
 			return;
 		}
-		
+
 		$out = array();
-		
-		foreach($rs as $a) {
+
+		foreach ($rs as $a)
+		{
 			$out[$a['name']] = $a['title'];
 		}
-		
+
 		return 
 			preg_replace(
 				'/<select[^>]*?>[\s\S]*?<\/select>/',
